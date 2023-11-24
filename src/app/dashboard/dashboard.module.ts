35640e71b2df7e5caf74759e90731827ccd5b787
camilo3900 <camilogv.1629@gmail.com>
2023-11-24 14:25:20 -0500

    Se crean los componentes navbar y toolbar del dashboard

--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -1,12 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ToolbarComponent } from './components/toolbar/toolbar.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { DashboardComponent } from './dashboard.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    ToolbarComponent,
+    SidebarComponent,
+    DashboardComponent
+  ],
   imports: [
     CommonModule
+  ],
+  exports:[
+    DashboardComponent
   ]
 })
 export class DashboardModule { }
